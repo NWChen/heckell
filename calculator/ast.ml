7a69693b73e447c810b5806ca56c748193af18cc
chan <tqp2001@columbia.edu>
2018-02-14 20:51:36 -0500

    updated TODOs

--- calculator/ast.ml
+++ calculator/ast.ml
@@ -1,29 +1,32 @@
 type operator = Add | Sub | Mul | Div
 
-(* type prim_typ = Int | Bool | Real | Char
- *)
+type prim_typ = Int | Bool | Real | Char
+
 type typ = 
     Set of typ
   | Int
 (*   | Unit of prim_typ
  *)
+
 type bind = string * typ
 
 type expr =
     Binop of expr * operator * expr
   | Lit of int
   | Decl of string * typ
-  | Seq of expr * expr 
   | Asn of string * expr
-  | Var of string
+  | Id of string
+  (* TODO: | Func of ... *)
+  (* | Seq of expr * expr  *)
 
  type stmt =
     Block of stmt list
   | Expr of expr
-  | Return of expr
-  | If of expr * stmt * stmt
-  | For of expr * expr * expr * stmt
-  | While of expr * stmt
+  (* | Return of expr *)
+  (* | If of expr * stmt * stmt *)
+  (* | For of expr * expr * expr * stmt *)
+  (* | While of expr * stmt *)
+
 
 (* TODO: operator for `->` (TYPE) *)
 (* TODO: operator for `(...)` (PARAMS) *)
