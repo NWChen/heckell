cd45d2d6907dc37afb4fe60f3bf6cb478b53fbf9
Sebastian Cueva <sc3972@columbia.edu>
2018-02-17 19:22:48 -0500

    finished moving pretty printing functions to their own file

--- heckell.ml
+++ heckell.ml
@@ -1,6 +1,6 @@
-open Ast
+open Pprinting
 
 let () = 
   let lexbuf = Lexing.from_channel stdin in
   let ast = Parser.program Scanner.tokenize lexbuf in
-  print_string (Ast.string_of_program ast)
+  print_string (Pprinting.string_of_program ast)
