250e67c86eb649cb5e6f63692f3f3347ef159f32
Neil Chen <neilwchen@gmail.com>
2018-03-26 23:13:02 +0000

    fix

--- heckell.sh
+++ heckell.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env
+#!/bin/bash
 
-./heckell < $1 | cat > temp.ll
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ./heckell.sh YOURFILE.hck"
+  exit 1 
+fi
+
+/heckell < $1 | cat > temp.ll
 llc temp.ll > temp.s
 cc -o temp.exe temp.s
 ./temp.exe
