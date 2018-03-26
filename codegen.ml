2b237c08cdb660be581bbff0f89846868ce24c1b
Neil Chen <neilwchen@gmail.com>
2018-03-26 16:51:11 +0000

    resolve A.typ -> L.lltype mismatch in function body generation

--- codegen.ml
+++ codegen.ml
@@ -27,7 +27,7 @@ let translate (statement_list) =
   (* Add types to the context so we can use them in our LLVM code *)
   let i32_t      = L.i32_type       context
   and i8_t       = L.i8_type        context 
-  and str_t      = L.pointer_type   i8_t
+  and str_t      = L.pointer_type   (L.i8_type context)
   (* Create an LLVM module -- this is a "container" into which we'll 
      generate actual code *)
   and the_module = L.create_module context "Heckell" in
@@ -36,7 +36,7 @@ let translate (statement_list) =
   let ltype_of_typ = function
       A.PrimTyp(Int) -> i32_t
     | A.String       -> str_t
-    | t -> raise (Failure ("Type " ^ string_of_prim_typ t ^ " not implemented yet"))
+    | _ -> raise (Failure ("Type not implemented yet"))
   in
 
   (* Declare a "printf" function to implement MicroC's "print". *)
@@ -50,7 +50,7 @@ let translate (statement_list) =
   (* Generate the instructions for a trivial "main" function *)
   let build_function fdecl =
     (* Make the LLVM module "aware" of the main function *)
-    let main_ty = L.function_type (ltype_of_typ A.Int) [||] in
+    let main_ty = L.function_type (ltype_of_typ (A.PrimTyp(Int))) [||] in
     let the_function = L.define_function "main" main_ty the_module in
     (* Create an Instruction Builder, which points into a basic block
       and determines where the next instruction should be placed *)
