809263c6cd00edd1ad07dfd95d9e9a98b723096b
Neil Chen <neilwchen@gmail.com>
2018-04-12 01:54:10 +0000

    include builtin function `print_string`

--- semant.ml
+++ semant.ml
@@ -164,6 +164,7 @@ let check stmts =
     | [] -> []
   in
   let symbols_init = StringMap.add "print" (Func(PrimTyp(Int), PrimTyp(Int))) StringMap.empty in
+  let symbols_init = StringMap.add "print_string" (Func(String, PrimTyp(Int))) symbols_init in
   let g_scope = {symb = symbols_init; parent = None} in 
   let symbols = check_stmt stmts g_scope
   in append_sstmt symbols stmts
