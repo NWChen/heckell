943a463b6391991378eb343f48bbbd2438e45e24
MargaretQian <myq2000@columbia.edu>
2018-04-17 00:32:53 -0400

    tests for test suite deliverable

--- testall.sh
+++ testall.sh
@@ -14,33 +14,41 @@ CC="cc"
 # Set time limit for all operations
 ulimit -t 30
 
+globallog=testall.log
+rm -f $globallog
+
 Compare() {
     if diff $1 $2 &> /dev/null ; then
-		echo "SUCCESS"
-	else
-		echo "FAIL"
-	fi
+        echo "SUCCESS"
+    else
+        echo "FAIL"
+    fi
 }
 
-files="tests/test-*.hck"
+files="tests/test-*.hck tests/fail-*.hck"
 
 for file in $files
 do
     basename=`echo $file | sed 's/.*\\///
-                         		s/.hck//'`
-	echo -n "$basename..."
+                                s/.hck//'`
+    echo -n "$basename..."
 
-    $HECKELL $file > "tests/${basename}.ll"
-    $LLC "tests/${basename}.ll" > "tests/${basename}.s"
-    $CC -o "tests/${basename}.exe" "tests/${basename}.s"
-    "./tests/${basename}.exe" > "tests/${basename}.diff"
+
 
     case $file in
-	*test-*)
-	    Compare tests/${basename}.diff tests/${basename}.out
-	    ;;
-	*)
-	    echo "unknown file type $file"
+    *test-*)
+        $HECKELL $file > "tests/${basename}.ll"
+        $LLC "tests/${basename}.ll" > "tests/${basename}.s"
+        $CC -o "tests/${basename}.exe" "tests/${basename}.s"
+        "./tests/${basename}.exe" > "tests/${basename}.diff"
+        Compare tests/${basename}.diff tests/${basename}.out
+        ;;
+    *fail-*)
+        $HECKELL $file 2> "tests/${basename}.diff"
+        Compare tests/${basename}.diff tests/${basename}.err
+        ;;
+    *)      
+        echo "unknown file type $file"
     esac
 
     # Remove generated file
